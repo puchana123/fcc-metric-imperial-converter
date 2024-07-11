9cc06801d36c40fd75c04ee49778dccece62d16d
Puchana <puchana123@gmail.com>
2024-07-11 15:13:20 +0700

    feat: add topic of each test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,76 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
+    suite('Read Input',()=>{
+        // #1
+        test('Read a whole number input', ()=>{
 
+        });
+        // #2
+        test('Read a decimal number input', ()=>{
+            
+        });
+        // #3
+        test('Read a fractional input', ()=>{
+            
+        });
+        // #4
+        test('Read a fractional input with a decimal', ()=>{
+            
+        });
+    });
+// -------------------------------------------------------------
+    suite('Input checking',()=>{
+        // #5 (i.e. 3/2/3 )
+        test('Return an error on a double fraction', ()=>{
+
+        });
+        // #6
+        test('If no numerical input, default should be 1', ()=>{
+            
+        });
+        // #7
+        test('Read each valid input unit', ()=>{
+            
+        });
+        // #8
+        test('Return an error for an invalid input unit', ()=>{
+            
+        });
+        // #9
+        test('Return unit correctly for each valid input unit', ()=>{
+            
+        });
+        // #10
+        test('Return the spelled-out string unit correctly for each valid input unit', ()=>{
+            
+        });
+    });
+// -------------------------------------------------------------
+    suite('Convert unit input',()=>{
+        // #11
+        test('Convert gal to L', ()=>{
+
+        });
+        // #12
+        test('Convert L to gal', ()=>{
+
+        });
+        // #13
+        test('Convert mi to km', ()=>{
+
+        });
+        // #14
+        test('Convert km to mi', ()=>{
+
+        });
+        // #15
+        test('Convert lbs to kg', ()=>{
+
+        });
+        // #16
+        test('Convert kg to lbs', ()=>{
+
+        });
+    });
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,25 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+    this.timeout(5000);
+    // #1 GET:/api/convert
+    test('Convert valid input : 10L', ()=>{
 
+    });
+    // #2 GET:/api/convert
+    test('Convert invalid input : 32g', ()=>{
+
+    });
+    // #3 GET:/api/convert
+    test('Convert invalid number : 3/7.2/4kg', ()=>{
+
+    });
+    // #4 GET:/api/convert
+    test('Convert invalid number AND unit : 3/7.2/4kilomegagram', ()=>{
+
+    });
+    // #5 GET:/api/convert
+    test('Convert with no number : kg', ()=>{
+
+    });    
 });
