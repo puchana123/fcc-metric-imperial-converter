e5003bbf7ad8b1179b59efedfe478d8467fd20f4
Puchana <puchana123@gmail.com>
2024-07-12 16:53:18 +0700

    feat: add convert function

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -73,12 +73,28 @@ function ConvertHandler() {
   };
 
   this.convert = function (initNum, initUnit) {
+    let result;
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
-    result = 'retrun number & unit'
-    return result;
+
+    switch (initUnit) {
+      case 'gal': result = initNum * galToL;
+        break;
+      case 'L': result = initNum / galToL;
+        break;
+      case 'mi': result = initNum * miToKm;
+        break;
+      case 'km': result = initNum / miToKm;
+        break;
+      case 'lbs': result = initNum * lbsToKg;
+        break;
+      case 'kg': result = initNum / lbsToKg;
+        break;
+      default: result = 'not valid unit'
+        break;
+    }
+    return Number(result.toFixed(5));
   };
 
   this.getString = function (initNum, initUnit, returnNum, returnUnit) {
