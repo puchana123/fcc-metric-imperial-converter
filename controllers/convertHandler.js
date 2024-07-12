ede06c508c6476f0e8f3a931675ff3b783786ccb
Puchana <puchana123@gmail.com>
2024-07-12 14:52:53 +0700

    feat: add getNum function

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,30 +1,36 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
+
+  this.getNum = function (input) {
     let result;
-    result = 'init number'
+    const divide_count = input.match(/\//g)
+    if (divide_count === null || divide_count.length === 1) {
+      result = eval(input.match(/^[0-9]+(\.[0-9]+)?(\/[0-9]+(\.[0-9]+)?)?/g)[0]);
+    } else {
+      return false
+    }
+
     return result;
   };
-  
-  this.getUnit = function(input) {
+
+  this.getUnit = function (input) {
     let result;
     result = 'init unit'
     return result;
   };
-  
-  this.getReturnUnit = function(initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
     let result;
     result = 'return unit'
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.spellOutUnit = function (unit) {
     let result;
     result = 'full unit string'
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
@@ -32,15 +38,15 @@ function ConvertHandler() {
     result = 'retrun number & unit'
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
     const returnUnitFull = this.spellOutUnit(returnUnit);
     const initUnitFull = this.spellOutUnit(initUnit);
     result = `${initNum} ${initUnitFull} converts to ${returnNum} ${returnUnitFull}`
     return result;
   };
-  
+
 }
 
 module.exports = ConvertHandler;
