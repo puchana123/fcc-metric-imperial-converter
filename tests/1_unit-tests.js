8eedf6a060be4995061819c4e84b596e8d0a0d47
Puchana <puchana123@gmail.com>
2024-07-12 17:13:31 +0700

    feat: add fail to all unfinish test cases

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -8,73 +8,75 @@ suite('Unit Tests', function(){
     suite('Read Input',()=>{
         // #1
         test('Read a whole number input', ()=>{
-
+            const whole_number = '12345';
+            const output = convertHandler.getNum(whole_number);
+            assert.equal(output, whole_number, "read all input numbers");
         });
         // #2
         test('Read a decimal number input', ()=>{
-            
+            assert.fail();
         });
         // #3
         test('Read a fractional input', ()=>{
-            
+            assert.fail();
         });
         // #4
         test('Read a fractional input with a decimal', ()=>{
-            
+            assert.fail();
         });
     });
 // -------------------------------------------------------------
     suite('Input checking',()=>{
         // #5 (i.e. 3/2/3 )
         test('Return an error on a double fraction', ()=>{
-
+            assert.fail();
         });
         // #6
         test('If no numerical input, default should be 1', ()=>{
-            
+            assert.fail();
         });
         // #7
         test('Read each valid input unit', ()=>{
-            
+            assert.fail();
         });
         // #8
         test('Return an error for an invalid input unit', ()=>{
-            
+            assert.fail();
         });
         // #9
         test('Return unit correctly for each valid input unit', ()=>{
-            
+            assert.fail();
         });
         // #10
         test('Return the spelled-out string unit correctly for each valid input unit', ()=>{
-            
+            assert.fail();
         });
     });
 // -------------------------------------------------------------
     suite('Convert unit input',()=>{
         // #11
         test('Convert gal to L', ()=>{
-
+            assert.fail();
         });
         // #12
         test('Convert L to gal', ()=>{
-
+            assert.fail();
         });
         // #13
         test('Convert mi to km', ()=>{
-
+            assert.fail();
         });
         // #14
         test('Convert km to mi', ()=>{
-
+            assert.fail();
         });
         // #15
         test('Convert lbs to kg', ()=>{
-
+            assert.fail();
         });
         // #16
         test('Convert kg to lbs', ()=>{
-
+            assert.fail();
         });
     });
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -9,22 +9,22 @@ suite('Functional Tests', function() {
     this.timeout(5000);
     // #1 GET:/api/convert
     test('Convert valid input : 10L', ()=>{
-
+        assert.fail();
     });
     // #2 GET:/api/convert
     test('Convert invalid input : 32g', ()=>{
-
+        assert.fail();
     });
     // #3 GET:/api/convert
     test('Convert invalid number : 3/7.2/4kg', ()=>{
-
+        assert.fail();
     });
     // #4 GET:/api/convert
     test('Convert invalid number AND unit : 3/7.2/4kilomegagram', ()=>{
-
+        assert.fail();
     });
     // #5 GET:/api/convert
     test('Convert with no number : kg', ()=>{
-
+        assert.fail();
     });    
 });
